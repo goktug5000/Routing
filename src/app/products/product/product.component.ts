4dea520a4fdc82f7c4e68ff466c4721b1d4066fb
goktug5000 <goktug5000@gmail.com>
2023-10-23 12:13:08 +0300

    ileri geri

--- src/app/products/product/product.component.ts
+++ src/app/products/product/product.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { products } from 'src/app/products';
 
 @Component({
@@ -13,7 +13,7 @@ export class ProductComponent implements OnInit{
   selectedProduct:any;
 
 
-  constructor(private route:ActivatedRoute){
+  constructor(private route:ActivatedRoute, private router:Router){
 
   }
 
@@ -21,7 +21,27 @@ export class ProductComponent implements OnInit{
 
 
   ngOnInit(): void {
-    let id=Number(this.route.snapshot.paramMap.get('id'));
-    this.selectedProduct=products.find(ii=>ii.id===id);
+    //let id=Number(this.route.snapshot.paramMap.get('id'));
+    //this.selectedProduct=products.find(ii=>ii.id===id);
+
+    this.route.paramMap
+    .subscribe(dnm=>
+      {
+      console.log("adres değişti");
+      let id = Number(dnm.get('id'));
+      this.selectedProduct=products.find(ii=>ii.id===id);
+      }
+    );
+
   }
+  goNextProduct(nextt?:boolean){
+    let id=Number(this.route.snapshot.paramMap.get('id'));
+    if(nextt==false){
+      this.router.navigate(['/products/'+String(id-1)]);
+      return
+    }
+    this.router.navigate(['/products/'+String(id+1)]);
+  }
+
+
 }
